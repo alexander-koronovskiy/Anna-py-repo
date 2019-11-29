cd58c0dd8ad5b979245df91b414d35254c2be659
alexander <alexander.koronovskiy@gmail.com>
2019-11-29 14:27:16 +0400

    1

---
+++ commented_code.py
@@ -0,0 +1,142 @@
+
+'''
+# программа, вычисляющая часы и минуты
+n=int(input())
+m=int(input())
+d=abs(n-m)//60
+k=abs(n-m)%60
+print(d,k)
+'''
+
+'''
+# песец 
+a=int(input())
+print(a//2)
+if (a//2):
+    print("Четное число")
+else:
+    print("НЕЧетное число")
+'''
+
+'''
+#максимальное число
+a=int(input())
+b=int(input())
+if (a>b):
+    print(a,"большее число")
+else:
+    print(b,"большее число")
+'''
+
+'''
+# first wrong program in 1.6
+a=input()
+b=input()
+c=input()
+d= input()
+if (a in d or b in d or c in d):
+    print("YES")
+else:
+    print("NO")
+'''
+''' 
+person = input()
+status = "Ты не пройдешь!"
+company = {"Арагорн", "Леголас", "Мериадок Брендибак", "Боромир", "Фродо Бэггинс"}
+if (person in company):
+    status = "Проходи!"
+print(status)
+'''
+
+'''
+if v > 0:
+    v = v ** 2
+else:
+     v = v ** 4
+if b > 0:
+    b = b ** 2
+else:
+    b = b ** 4
+if c > 0:
+     c = c ** 2
+else:
+     c = c ** 4
+'''
+'''
+# задача 1
+v=int(input())
+b=int(input())
+c=int(input())
+if v > 0:
+    v = v ** 2
+else:
+     v = v ** 4
+if b > 0:
+    b = b ** 2
+else:
+    b = b ** 4
+if c > 0:
+     c = c ** 2
+else:
+     c = c ** 4
+print(v, b, c)
+'''
+
+"""
+#Задача 4 
+a=int(input())
+b=int(input())
+c=a+b
+if c<180:
+    print("Треугольник существует")
+    if (c==90):
+        print("Прямоугольный треугольник")
+    else:
+        print("Не пряумогольный треугольник")
+else:
+    print("Нет такого треугольника")
+"""
+
+"""
+# задача 2
+a=int(input())
+b=int(input())
+c=int(input())
+if a == b == c and a!=0 and b!=0 and c!==0:
+    print("Равностороний треугольник")
+else:
+    print("Не равносторонний треугольник")
+"""
+''' 
+# Задача 5
+a=int(input())
+b=a//100
+c=a%100//10
+d=a%10
+h=b+c+d
+l=h%2==0
+print(l)
+'''
+"""
+# Задача 6
+a=int(input())
+b=int(input())
+c=int(input())
+if a==b and a!=0 and b!=0 and c!=0 :
+    print("Равнобедренный треугольник")
+else:
+   print ("Не равнобедренный треугольник")
+"""
+""" 
+# Задача 3
+a=int(input())
+a1= a//10
+a2=a%100//10
+a3=a%100
+b=a**2
+c=a1+a2+a3
+if (b==c):
+    print("равен")
+else:
+    print("Не равен")
+"""
---
+++ first.py
@@ -0,0 +1,30 @@
+def vector_v_complex(dv_massiv):
+    complex_num = list()
+    for i in range(len(dv_massiv)):
+        complex_num.append(complex(dv_massiv[i][0], dv_massiv[i][1]))
+    return complex_num
+
+
+# a = [[1, 2], [3, 4], [5, 6]]
+# b = vector_v_complex(a)
+
+# ввод числа, преобразование типа в int
+a = int(input())
+
+# находит цифры числа
+c = a % 10
+b = a % 100 // 10
+d = a // 100
+
+# находит максимальную цифру числа
+if(b > c):
+    if (b > d):
+        print(b)
+elif(c > b):
+    if(c > d):
+            print(c)
+elif(d > b):
+    if (d > c):
+        print(d)
+elif(c == b == d):
+    print(c)
